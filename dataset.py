a4e68dc8fab25b4fd8379df313246790ac55c841
Fuad Mammad <fred.mmov@gmail.com>
2026-07-28 03:36:47 +0200

    model: baseline CNN + Dataset class

---
+++ dataset.py
@@ -0,0 +1,44 @@
+from scipy.io import loadmat
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class GazeDataset(Dataset):
+    def __init__(self, mat_path):
+        data = loadmat(mat_path, squeeze_me=True, struct_as_record=False)
+        
+        self.images = data["data"].right.image
+        self.gaze = data["data"].right.gaze
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, i):
+        image = self.images[i]
+        image = torch.from_numpy(image).float() / 255.0 
+        image = image.unsqueeze(0)
+
+        x, y, z = self.gaze[i]
+        pitch = np.arcsin(-y)
+        yaw = np.arctan2(-x, -z)
+
+        label = torch.tensor([pitch, yaw], dtype=torch.float32)
+        return image, label
+    
+    
+
+#Testing
+mat_path = "./dataset/Data/Normalized/p00/day01.mat"
+
+dataset = GazeDataset(mat_path)
+
+print(len(dataset))
+
+img, label = dataset[0]
+print(img.shape, img.dtype)
+print(label.shape)
+
+loader = DataLoader(dataset, batch_size=16, shuffle=False)
+
+images, labels = next(iter(loader))
+print(images.shape, labels.shape)
---
+++ pipeline.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from dataset import GazeDataset
+
+class GazeCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=5)
+        self.pool = nn.MaxPool2d(kernel_size=2)
+        self.fc1 = nn.Linear(16*6*12, 16)
+        self.fc2 = nn.Linear(16, 2)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = torch.relu(x)
+        x = self.pool(x)
+
+        x = self.conv2(x)
+        x = torch.relu(x)
+        x = self.pool(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = torch.relu(x)
+        x = self.fc2(x)
+        return x
+
+mat_path = "./dataset/Data/Normalized/p00/day01.mat"
+dataset = GazeDataset(mat_path)
+loader = DataLoader(dataset, batch_size=16, shuffle=False)
+images, labels = next(iter(loader))
+
+net = GazeCNN()
+out = net(images)
+
+print(out.shape)
+ 
+
+
